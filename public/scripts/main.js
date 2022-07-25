0fedd6cbef5032f6cf0858a1bdc548c6cda7a7d3
Nat-1501 <natalisoares90@gmail.com>
2022-07-24 21:38:36 -0300

    realizada o close da modal

---
+++ public/scripts/main.js
@@ -0,0 +1,17 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+
+//Pegar todos os botões que existe com a classe check
+const checkButtons = document.querySelectorAll(".actions a.check") 
+
+checkButtons.forEach(button => {
+    //adicionar a escuta
+    button.addEventListener("click", event => {
+        //Abrir modal
+        modal.open() 
+    })
+})
+
+
+
