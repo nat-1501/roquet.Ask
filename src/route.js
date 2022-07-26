36bf09f12ab94f6143b583450c13a78236a882aa
Nat-1501 <natalisoares90@gmail.com>
2022-07-26 09:21:25 -0300

    Ajustando hrefs

--- src/route.js
+++ src/route.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
-//const RoomController = require('./controllers/RoomController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
 
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
-route.get('/create-pass', (req, res) => res.render("create-pass", {page: 'create-pass'}))
+route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
 route.get('/room', (req, res) => res.render("room"))
 
 
-//route.post('/create-room', RoomController.create)
+route.post('/room/create-room', RoomController.create)
 //route.get('/room/:room', RoomController.open)
 //route.post('/enterroom', RoomController.enter)
 
