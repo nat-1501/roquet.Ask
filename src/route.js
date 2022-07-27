40e12d2209c241496abd38cc48fb83a72d00d8dd
Nat-1501 <natalisoares90@gmail.com>
2022-07-27 14:33:00 -0300

    id room via get na url

--- src/route.js
+++ src/route.js
@@ -8,7 +8,7 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
-route.get('/room', (req, res) => res.render("room"))
+route.get('/room/:room', (req, res) => res.render("room"))
 
 
 route.post('/room/create-room', RoomController.create)
