a83bd8d0dff895c168af9e82161d3f91951f7016
Nat-1501 <natalisoares90@gmail.com>
2022-08-01 19:56:27 -0300

    Utilizando javascript para alterar o bot]ao com o Id da room

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -33,5 +33,23 @@ module.exports = {
         await db.close()
 
         res.redirect(`/room/${roomId}`)
+    },
+
+    open(req, res) {
+        const roomId = req.params.room
+        res.render("room", {roomId})
     }
+
+    // async open(req, res){
+    //     const db = await Database()
+    //     const roomId = req.params.room
+    //     const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+    //     const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+    //     let isNoQuestions
+
+    //     if(questions.length ==0){
+    //         if(questionsRead.length == 0){
+    //             isNoQuestions = true
+    //         }
+    //     }
 }
