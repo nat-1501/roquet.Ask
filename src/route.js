6dd35853f55c4aad6642982244b6ce6dadd1314c
Nat-1501 <natalisoares90@gmail.com>
2022-08-20 13:03:40 -0300

    ajustando criação de usuario

--- src/route.js
+++ src/route.js
@@ -13,11 +13,10 @@ route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'
 route.post('/create-room', RoomController.create)
 route.get('/room/:room', RoomController.open)
 route.post('/enterroom', RoomController.enter)
-// route.get('/enterusuario', RoomController.open)
 
 route.post('/question/create/:room', QuestionController.create)
 route.post('/question/:room/:question/:action', QuestionController.index)
 
-//teste git
+
 
 module.exports = route
